06773abffa862ac58137d69bdaf756fdc916af41
Selva08 <59572071+selva08@users.noreply.github.com>
2020-01-12 19:03:49 +0400

    Updates

---
+++ precache-manifest.314c8fd53ac1f022da4bfb6d3786b440.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b822b02a2d4d67296644a0e835ef370e",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "3eddb803ce6a03c8e942",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "c35209b422a60ba88379",
+    "url": "/robofriends/static/css/main.54791ebc.chunk.css"
+  },
+  {
+    "revision": "3eddb803ce6a03c8e942",
+    "url": "/robofriends/static/js/2.d2f9d004.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.d2f9d004.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c35209b422a60ba88379",
+    "url": "/robofriends/static/js/main.9ebe0320.chunk.js"
+  },
+  {
+    "revision": "e6e9e20fe23598f77326",
+    "url": "/robofriends/static/js/runtime-main.d4e5f5ba.js"
+  },
+  {
+    "revision": "f41bf827aced25ea51a89b6b262e52aa",
+    "url": "/robofriends/static/media/OstrichSansBold.f41bf827.woff"
+  }
+]);
